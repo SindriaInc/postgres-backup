87909fcb3693e5e197e1bf115937720ae5aff8e2
lucapitzoi <luca.pitzoi@sindria.org>
2021-05-23 23:00:48 +0200

    Fix

--- src/resources/bin/entrypoint.sh
+++ src/resources/bin/entrypoint.sh
@@ -4,36 +4,30 @@ set -e
 
 NOW=$(date "+%Y-%m-%d_%H-%M-%S")
 
-env
-
 # Setup pgdump
 touch /root/.pgpass
 
+# Build placeholder string
 STRING="*:*:*:"
 STRING+=${DB_USERNAME}
 STRING+=":"
-STRING+="##DB_PASSWORD##"
+STRING+="@@DB_PASSWORD@@"
 
+# Overwrite placeholder string
 echo "${STRING}" > /root/.pgpass
 
-cat /root/.pgpass
-
+# Overwrite unclean db password
 echo ${DB_PASSWORD} > unclean.txt
 
-cat unclean.txt
-
+# Cleanup db password colon and & operator
 sed -i -e 's/:/\\\\:/g' unclean.txt
 sed -i -e 's/&/\\&/g' unclean.txt
-cat unclean.txt
 
-#sed -ie "s/@@DB_PASSWORD@@/$(sed -e 's/:/\\:/g' unclean.txt)/g" /root/.pgpass
+# Find and replace db password placeholder with real cleaned db password
+sed -i -E "s|@@DB_PASSWORD@@|$(cat unclean.txt)|g" /root/.pgpass
 
-sed -i -E "s|##DB_PASSWORD##|$(cat unclean.txt)|g" /root/.pgpass
-
-cat /root/.pgpass
+# Setting permission
 chmod 600 /root/.pgpass
-
-ls -la /root/.pgpass
 
 # Dump scheme
 pg_dump -U ${DB_USERNAME} -h ${DB_HOST} -p ${DB_PORT} -d ${DB_NAME} -w -f ${APP_NAME}_${NOW}.sql
